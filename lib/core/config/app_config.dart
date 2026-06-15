e3b8efad59ee6296d3ae1ea5c2c1b22f350843c6
forworks286-star <forworks286@gmail.com>
2026-06-15 21:20:44 +0100

    Update app_config.dart

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   AppConfig._();
 
-  static const String _baseUrl = 'http://192.168.1.100:8000';
+  static const String _baseUrl = 'https://synexia-backend-production.up.railway.app';
 
   static const String apiBase = '$_baseUrl/api/v1';
 
